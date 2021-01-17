e370047695cb4155a9984ab50ffb3fb041b1a686
Ritwaja <76560475+Ritwaja@users.noreply.github.com>
2021-01-17 12:21:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,12 +6,12 @@ var together;
 function preload() {
     //load the images here
    bg=loadImage("garden.png");
-   tomImage1=loadAnimation("images/tomOne.png");
-   tomImage2=loadAnimation("images/tomTwo.png","images/tomThree.png");
-   tomImage3=loadAnimation("images/tomFour.png");
-   jerryImage1=loadAnimation("images/jerryOne.png");
-   jerryImage2=loadAnimation("images/jerryTwo.png","images/jerryThree.png");
-   jerryImage3=loadAnimation("images/jerryFour.png");
+   tomImage1=loadAnimation("tomOne.png");
+   tomImage2=loadAnimation("tomTwo.png","tomThree.png");
+   tomImage3=loadAnimation("tomFour.png");
+   jerryImage1=loadAnimation("jerryOne.png");
+   jerryImage2=loadAnimation("jerryTwo.png","jerryThree.png");
+   jerryImage3=loadAnimation("jerryFour.png");
 
    
     
@@ -21,11 +21,11 @@ function setup(){
     canvas = createCanvas(1000,800);
 
     //create tom and jerry sprites here
-     tom=createSrite(870,600);
+     tom=createSprite(870,600);
      tom.addAnimation("tomSleeping",tomImage1);
      tom.scale=0.2;
 
-     jerry=createSrite(200,600);
+     jerry=createSprite(200,600);
      jerry.addAnimation("jerryStanding",jerryImage1);
      jerry.scale=0.15;
 
